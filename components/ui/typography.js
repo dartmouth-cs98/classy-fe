cafc1505c2ecd5b8b758bdd11e453ff93e7cd95f
Alex Feng <alex.feng.23@dartmouth.edu>
2022-10-30 08:22:36 -0400

    laid out search page text components

--- components/ui/typography.js
+++ components/ui/typography.js
@@ -6,154 +6,133 @@ import styled from "styled-components"
 // Headings
 
 /* Asap/H1 */
-const StyledH1 = styled.h1`
-  color: var(--navy);
+export const H1 = styled.h1`
+  color: ${props => props.color ? props.color : "var(--navy)"};  // default navy color, color prop if specified
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 700;
   font-size: 64px;
   line-height: 73px;
 `
-export const H1 = ({ children }) => (
-  <StyledH1>{children}</StyledH1>
-)
 
 /* Asap/H2 */
-const StyledH2 = styled.h2`
-  color: var(--navy);
+export const H2 = styled.h2`
+  color: ${props => props.color ? props.color : "var(--navy)"};  
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 700;
   font-size: 42px;
   line-height: 48px;
 `
-export const H2 = ({ children }) => (
-  <StyledH2>{children}</StyledH2>
-)
 
 /* Asap/H3 */
-const StyledH3 = styled.h3`
-  color: var(--navy);
+export const H3 = styled.h3`
+  color: ${props => props.color ? props.color : "var(--navy)"};  
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 700;
   font-size: 22px;
   line-height: 143.01%;
 `
-export const H3 = ({ children }) => (
-  <StyledH3>{children}</StyledH3>
-)
 
 /* Asap/H4 */
-const StyledH4 = styled.h4`
-  color: var(--navy);
+export const H4 = styled.h4`
+  color: ${props => props.color ? props.color : "var(--navy)"};  
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 600;
   font-size: 18px;
   line-height: 143.01%;
 `
-export const H4 = ({ children }) => (
-  <StyledH4>{children}</StyledH4>
-)
 
 /* Asap/H5 */
-const StyledH5 = styled.h5`
-  color: var(--navy);
+export const H5 = styled.h5`
+  color: ${props => props.color ? props.color : "var(--navy)"};  
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 600;
   font-size: 16px;
   line-height: 115%;
 `
-export const H5 = ({ children }) => (
-  <StyledH5>{children}</StyledH5>
-)
 
 
 
 // Body
 
 /* Asap/Paragraph 1 */
-const StyledB1 = styled.b`
-  color: var(--navy);
+export const B1 = styled.b`
+  color: ${props => props.color ? props.color : "var(--navy)"};  
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
   line-height: 122.13%;
 `
-export const B1 = ({ children }) => (
-  <StyledB1>{children}</StyledB1>
-)
 
 /* Asap/Paragraph 2 */
-const StyledB2 = styled.b`
-  color: var(--navy);
+export const B2 = styled.b`
+  color: ${props => props.color ? props.color : "var(--navy)"};  
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 125.63%;
 `
-export const B2 = ({ children }) => (
-  <StyledB2>{children}</StyledB2>
-)
 
 /* Asap/Paragraph 3 */
-const StyledB3 = styled.b`
-  color: var(--navy);
+export const B3 = styled.b`
+  color: ${props => props.color ? props.color : "var(--navy)"};
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 400;
   font-size: 12px;
   line-height: 125.63%;
 `
-export const B3 = ({ children }) => (
-  <StyledB3>{children}</StyledB3>
-)
 
 
 
 // Other
 
 /* Asap/Text Label */
-const StyledTextLabel = styled.b`
-  color: var(--navy);
+export const TextLabel = styled.b`
+  color: ${props => props.color ? props.color : "var(--navy)"};
+  // color: var(--navy);
   font-family: 'Asap';
   font-style: normal;
   font-weight: 700;
   font-size: 14px;
   line-height: 163.44%;
+
+  text-transform: uppercase;
 `
-export const TextLabel = ({ children }) => (
-  <StyledTextLabel>{children}</StyledTextLabel>
-)
 
 /* Asap/Links */
-const StyledLink = styled.a`
-  color: var(--navy);
+export const A = styled.a`
+  color: ${props => props.color ? props.color : "var(--navy)"};
   font-family: 'Asap';
   font-style: normal;
-  font-weight: 400;
+  font-weight: 700;
   font-size: 14px;
-  line-height: 137.52%;
+  line-height: 163.4%;
 
   text-decoration-line: underline;
+  text-transform: uppercase;
 `
-export const A = ({ children }) => (
-  <StyledLink>{children}</StyledLink>
-)
 
 /* Asap/Menu */
-const StyledMenuText = styled.b`
-  color: var(--navy);
+export const MenuText = styled.b`
+  color: ${props => props.color ? props.color : "var(--navy)"};
   font-family: 'Asap';
   font-style: normal;
   font-weight: 400;
   font-size: 22px;
   line-height: 143.01%;
 `
-export const MenuText = ({ children }) => (
-  <StyledMenuText>{children}</StyledMenuText>
-)
--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { H1 } from "../components/ui/typography"
+import { H1, H2, H3, B1, B3, TextLabel } from "../components/ui/typography"
 
 const SearchPage = () => {
     const [searchInput, setSearchInput] = useState("")
@@ -14,13 +14,24 @@ const SearchPage = () => {
     return (
         <div>
             <input
+            autoFocus
             type="text"
             placeholder="Search"
             onChange={handleChange}
             value={searchInput}
             />
-            <h3>Recent Searches</h3>
-            <H1>hello test</H1>
+
+
+            <H3>Recent Searches</H3>
+            <TextLabel>See All</TextLabel>
+            {/* Load course cards */}
+            <CourseCard id="Cosc 98" title="Introductory Economics" distrib="TLA" quality="4.0" reviews="4" />
+
+            <H3>Browse Departments</H3>
+            {/* Load all departments */}
+            <DepartmentCard name="Computer Science" id="COSC"/>
+
+
         </div>
     )
       
@@ -31,8 +42,34 @@ const SearchPage = () => {
     //   })
 }
 
-const CourseCard = () => {
+const CourseCard = (props) => {
+    return (
+        <div>
+            <TextLabel color="var(--dark-grey)">{props.distrib}</TextLabel>
 
+            <div>
+                <TextLabel color="var(--darkest-grey)">Quality</TextLabel>
+                <H1 color="var(--dark-orange)">{props.quality}</H1>
+                <B3 color="var(--dark-grey)">{props.reviews} Reviews</B3>
+                
+                <H3>{props.id}</H3>
+                <B1>{props.name}</B1>
+            </div>
+
+
+        </div>
+    )
+}
+
+const DepartmentCard = (props) => {
+    return (
+        <div>
+            <H3 color="var(--dark-orange)">{props.name}</H3>
+            <H2 color="var(--dark-orange)">{props.id}</H2>
+
+
+        </div>
+    )
 }
 
 export default SearchPage
