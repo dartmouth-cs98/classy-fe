0dc3b56f7b1b91f43ed0f303aa4e527b423d9b50
gyur2hwng <gyuri.hwang.23@dartmouth.edu>
2022-10-30 22:21:21 -0400

    reset to js, explore page, start navbar

---
+++ components/SideNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {GiHamburgerenu} from "react-icons/gi"
+
+function SideNavbar() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default SideNavbar
--- pages/explore.tsx
+++
@@ -1,6 +0,0 @@
-function ExplorePage() {
-return <h1>Explore Page</h1>
-}
-
-export default ExplorePage
-
