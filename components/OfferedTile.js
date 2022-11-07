a25f1a35c89c5bbbd0714c81c53004ad8d4753d3
Henry Kim <henry.kim.23@dartmouth.edu>
2022-11-06 20:53:40 -0500

    flip term and prof

--- components/OfferedTile.js
+++ components/OfferedTile.js
@@ -5,8 +5,8 @@ import stylesCI from '../styles/CourseInfo.module.css'
 function OfferedTile(props) {
     return (
         <div className={stylesCI.tile} style={{background: getColor("term", props.term)}}>
-            <div className={stylesCI.tileText}>{props.instructors}</div>
             <div className={stylesCI.tileText}>{props.term}</div>
+            <div className={stylesCI.tileText}>{props.instructors}</div>
         </div>
     );
 }
