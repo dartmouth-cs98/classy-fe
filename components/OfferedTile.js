7e83c514130a1617bedd949c832ba7451315721a
Henry Kim <henry.kim.23@dartmouth.edu>
2022-11-09 16:48:41 -0500

    refactor data files

--- components/OfferedTile.js
+++ components/OfferedTile.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import getColor from '../pages/courseinfo/colorscheme';
+import getColor from '../data/colorscheme';
 import stylesCI from '../styles/CourseInfo.module.css';
 
 function OfferedTile(props) {
