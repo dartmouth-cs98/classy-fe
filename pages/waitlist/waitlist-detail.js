3cae3986e8a38a8319a30ee42cbfa36a7ab3a9ca
tradewinds23 <alyssa.k.anderson.23@dartmouth.edu>
2022-11-12 13:44:59 -0500

    Updated Waitlist pages
    Updated waitlist formatting and styling, and started to add dynamic elements
    
    Signed-off-by: tradewinds23 <alyssa.k.anderson.23@dartmouth.edu>

--- pages/waitlist/waitlist-detail.js
+++ pages/waitlist/waitlist-detail.js
@@ -3,8 +3,15 @@ import React from 'react';
 import Head from 'next/head';
 import RemoveWaitlist from '../../components/RemoveWaitlist';
 import styles from '../../styles/WaitlistDetail.module.css';
+import CourseData from '../../data/data';
+import WaitlistData from '../../data/waitlistdata';
+import { Link } from 'react-router-dom';
 
 export default function WaitlistDetail() {
+  const data = CourseData();
+  const waitlistData = WaitlistData();
+  const waitlist = waitlistData['COSC 52'];
+  const currentCourse = data['COSC 52'];
   return (
     <div className={styles.container}>
       <Head>
@@ -18,8 +25,12 @@ export default function WaitlistDetail() {
           My Waitlists
         </h1>
         <div className={styles.course_title}>
-          <h1>COSC 052</h1>
-          <h3>Full-Stack Web Development</h3>
+          <h1>
+            {currentCourse.course_code.course_dept}
+            {' '}
+            {currentCourse.course_code.course_number}
+          </h1>
+          <h3>{currentCourse.course_title}</h3>
         </div>
       </div>
 
@@ -27,25 +38,51 @@ export default function WaitlistDetail() {
         <div className={styles.left_info}>
           <div className={styles.waitlist_btns}>
             <RemoveWaitlist />
-            <button type="button" className={styles.button}>Edit Waitlist Request</button>
-            <button type="button" className={styles.button}>Course Info Page</button>
+            {/* <button type="button" className={styles.button}>Edit Waitlist Request</button> */}
+            <a href="/courseinfo" className={styles.button}>Course Info Page</a>
+            {/* <button type="button" className={styles.button}>Course Info Page</button> */}
           </div>
 
           <div className={styles.waitlist_details_container}>
             <div className={styles.waitlist_element}>
-              <h2>Estimated terms remaining: 2 terms</h2>
+              <h2>
+                Estimated terms remaining:
+                {' '}
+                {waitlist.remaining_terms}
+                {' '}
+                terms
+              </h2>
             </div>
             <div className={styles.waitlist_element}>
-              <h2>Average time spent on waitlist: 4 terms</h2>
-              <h2>Joined the waitlist: 10 June 22</h2>
+              <div>
+                <h2>
+                  Average time spent on waitlist:
+                  {' '}
+                  {waitlist.avg_terms}
+                  {' '}
+                  terms
+                </h2>
+              </div>
+              <div>
+                <h2>
+                  Joined the waitlist:
+                  {' '}
+                  {waitlist.joined}
+                </h2>
+              </div>
             </div>
           </div>
         </div>
 
         <div className={styles.right_info}>
-
           <div className={styles.waitlist_position}>
-            <div className={styles.info_graphic}>10 / 76</div>
+            <div className={styles.info_graphic}>
+              <h1>
+                {waitlist.waitlist_pos}
+                {' / '}
+                {waitlist.waitlist_total}
+              </h1>
+            </div>
             <p>waitlist position</p>
           </div>
 
@@ -54,7 +91,7 @@ export default function WaitlistDetail() {
               photo
             </div>
             <h3>Professor Jane</h3>
-            <button type="button" className={styles.email_btn}>
+            <button type="button" className={styles.button}>
               Email
             </button>
           </div>
