d8f845388d01808075f3b48ff9d0724a655f0b9c
tradewinds23 <alyssa.k.anderson.23@dartmouth.edu>
2022-11-13 21:33:07 -0500

    Waitlist organization and page updates
    Changed waitlist pages to be accessible by dept and course number from data files. Removed unnecessary buttons and updated formatting, added waitlist data file to currently store waitlist position and relevant waitlist information.
    
    Signed-off-by: tradewinds23 <alyssa.k.anderson.23@dartmouth.edu>

--- data/waitlistdata.js
+++ data/waitlistdata.js
@@ -1,17 +1,37 @@
 export default function WaitlistData() {
   const cs52 = {
-    course_code: {
-      course_dept: 'COSC',
-      course_number: '52',
-    },
     waitlist_pos: 10,
     waitlist_total: 76,
     remaining_terms: 2,
     avg_terms: 4,
-    joined: 'June 11 2022',
+    joined: 'June 21 2022',
+  };
+  const cs74 = {
+    waitlist_pos: 2,
+    waitlist_total: 14,
+    remaining_terms: 1,
+    avg_terms: 2,
+    joined: 'April 10 2022',
+  };
+  const math1 = {
+    waitlist_pos: 5,
+    waitlist_total: 7,
+    remaining_terms: 1,
+    avg_terms: 1,
+    joined: 'April 10 2022',
+  };
+  const econ1 = {
+    waitlist_pos: 32,
+    waitlist_total: 35,
+    remaining_terms: 2,
+    avg_terms: 2,
+    joined: 'November 11 2022',
   };
   const courses = {
     'COSC 52': cs52,
+    'COSC 74': cs74,
+    'MATH 001': math1,
+    'ECON 001': econ1
   };
   return courses;
 }
