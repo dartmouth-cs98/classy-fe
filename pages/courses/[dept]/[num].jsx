457b8d9c9ae0ba1e75492bf77bedb27c005e8433
Henry Kim <henry.kim.23@dartmouth.edu>
2022-11-13 21:34:15 -0500

    revert fe be integration

--- pages/courses/[dept]/[num].jsx
+++ pages/courses/[dept]/[num].jsx
@@ -2,23 +2,22 @@ import React from 'react';
 
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { connect } from 'react-redux';
-import styles from '../../../styles/Home.module.css';
+// import { useDispatch, connect } from 'react-redux';
 import CourseData from '../../../data/data';
 import Glance from '../../../components/Glance';
 import CourseInfoTitle from '../../../components/CourseInfoTitle';
 import Offered from '../../../components/Offered';
 import Medians, { convertMedian } from '../../../components/Medians';
 import StudentsSay from '../../../components/StudentsSay';
-import { fetchCourse } from '../../../actions';
+// import { fetchCourse } from '../../../actions';
 import getPrereqs from '../../../data/courseinfohelpers';
+import SideNavbar from '../../../components/SideNavbar';
 
-// import SideNavbar from '../../components/SideNavbar';
 import {
   H3, B1,
 } from '../../../components/ui/typography';
 
-function CourseInfo() {
+export default function CourseInfo() {
   const data = CourseData();
   const router = useRouter();
   const { dept, num } = router.query;
@@ -27,51 +26,48 @@ function CourseInfo() {
   const currentCourse = data[courseCode];
 
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Classy</title>
         <meta name="description" content="class selection made easy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <SideNavbar /> */}
-      <CourseInfoTitle course={currentCourse || { dept, num }} />
+      <SideNavbar />
 
-      <H3>Description</H3>
-      <B1>{currentCourse ? currentCourse.description : ''}</B1>
+      <main>
+        <CourseInfoTitle course={currentCourse || { dept, num }} />
 
-      <H3>At a Glance</H3>
-      <Glance
-        distribs={currentCourse ? currentCourse.distribs : ''}
-        wc={currentCourse ? currentCourse.wc : ''}
-        avgMedian={currentCourse ? convertMedian(currentCourse.avgMedian) : ''}
-        waitlist={currentCourse ? currentCourse.waitlist : 'Unknown'}
-        dept={currentCourse ? currentCourse.dept : ''}
-        num={currentCourse ? currentCourse.num : ''}
-      />
+        <H3>Description</H3>
+        <B1>{currentCourse ? currentCourse.description : ''}</B1>
 
-      <H3>Prerequisites</H3>
-      {currentCourse ? getPrereqs(currentCourse.required, currentCourse.counts) : ''}
+        <H3>At a Glance</H3>
+        <Glance
+          distribs={currentCourse ? currentCourse.distribs : ''}
+          wc={currentCourse ? currentCourse.wc : ''}
+          avgMedian={currentCourse ? convertMedian(currentCourse.avgMedian) : ''}
+          waitlist={currentCourse ? currentCourse.waitlist : 'Unknown'}
+          dept={currentCourse ? currentCourse.dept : ''}
+          num={currentCourse ? currentCourse.num : ''}
+        />
 
-      <H3>What Students Say</H3>
-      <StudentsSay
-        workload={currentCourse ? currentCourse.workload : 'Unknown'}
-        difficulty={currentCourse ? currentCourse.difficulty : 'Unknown'}
-        quality={currentCourse ? currentCourse.quality : 'Unknown'}
-      />
+        <H3>Prerequisites</H3>
+        {currentCourse ? getPrereqs(currentCourse.required, currentCourse.counts) : ''}
 
-      <H3>Offered</H3>
-      {currentCourse ? <Offered course={currentCourse} /> : <B1>No Data</B1>}
+        <H3>What Students Say</H3>
+        <StudentsSay
+          workload={currentCourse ? currentCourse.workload : 'Unknown'}
+          difficulty={currentCourse ? currentCourse.difficulty : 'Unknown'}
+          quality={currentCourse ? currentCourse.quality : 'Unknown'}
+        />
 
-      <H3>Medians</H3>
-      {currentCourse ? <Medians medians={currentCourse.medians} /> : <B1>No Data</B1>}
-      {/* <h2 className={styles.title}>Reviews</h2> */}
+        <H3>Offered</H3>
+        {currentCourse ? <Offered course={currentCourse} /> : <B1>No Data</B1>}
+
+        <H3>Medians</H3>
+        {currentCourse ? <Medians medians={currentCourse.medians} /> : <B1>No Data</B1>}
+        {/* <h2 className={styles.title}>Reviews</h2> */}
+      </main>
     </div>
   );
 }
-
-function mapStateToProps(reduxState) {
-  return { currentPost: reduxState.courses.current };
-}
-
-export default connect(mapStateToProps, { fetchCourse })(CourseInfo);
