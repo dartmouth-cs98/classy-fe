19edef43e077d4362d7353a8c92535be7c982f5d
Henry Kim <henry.kim.23@dartmouth.edu>
2022-11-17 22:39:00 -0500

    attempt to solve netlify error with import professorcard

--- components/professors/ProfessorCard.jsx
+++ components/professors/ProfessorCard.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {
   H1, B1, TextLabel,
 } from '../ui/typography';
+// eslint-disable-import/no-unresolved
 import styles from '../../styles/components/ProfessorCard.module.css';
 
 function ProfessorCard(props) {
   const { name, departments } = props;
   return (
+
     <div className={styles.container}>
       <img className={styles.pic} src="https://faculty-directory.dartmouth.edu/sites/faculty_directory.prod/files/styles/profile_portrait/public/profile_square.jpg?itok=lVqJtQt6" alt="Tim" />
       <div className={styles.wordSection}>
