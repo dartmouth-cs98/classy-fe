294451acf7bdb142d3219a5122744716b8b563d9
Vi-N-Tran <Vi.n.tran.23@dartmouth.edu>
2023-01-25 22:07:10 -0500

    configured nextjs image for classy s3

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['classy-api-bucket.s3.amazonaws.com', 's3.amazonaws.com'],
   },
-};
-
-// const imagesConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'classy-api-bucket.s3.amazonaws.com',
-//         port: '',
-//         pathname: '**',
-//       },
-//     ],
-//   },
-// };
-
-module.exports = {
-  nextConfig,
-  // imagesConfig,
 };
