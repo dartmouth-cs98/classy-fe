49e6c9b4874adb074c4d24b29343c6a5ef83c19b
Vi-N-Tran <Vi.n.tran.23@dartmouth.edu>
2023-02-05 15:49:00 -0500

    social pay layout

---
+++ constants/colors.jsx
@@ -0,0 +1,7 @@
+export const cardColors = [
+  { pastel: '#FCF0E3', dark: '#BA7D37' },
+  { pastel: '#EBF9FA', dark: '#5B8A8D' },
+  { pastel: '#EFFAEB', dark: '#75946A' },
+];
+
+// This file will contain arrays of all pastel and dark colors
