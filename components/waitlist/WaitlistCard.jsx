e2df7c50b5f7cd2b04b80e85521cbd34e376ebd1
tradewinds23 <alyssa.k.anderson.23@dartmouth.edu>
2023-02-13 13:46:14 -0500

    Waitlist Tile Update
    restored waitlist tile formatting from pr 487e478. Single waitlist dashboard with relevant waitlist/course information displayed in tiles. Lists current terms on the waitlist as well as the current position for the upcoming term. Fixed CSS bug, tile scale issue, data, and updated the tile to show the correct information. Not connected to backend, instead getting mock waitlist data passed in from waitlist>index.js.
    Known bug: tiles not displaying in grid, but  instead as a single column

---
+++ components/waitlist/WaitlistCard.jsx
@@ -0,0 +1,92 @@
+/* eslint-disable no-unused-vars */
+// import { makeStyles } from '@material-ui/core/styles';
+// import RemoveWaitlist from './RemoveWaitlist';
+import React from 'react';
+// import { useState } from 'react';
+import { TextField } from '@mui/material';
+import {
+  MdOutlineSettings,
+} from 'react-icons/md';
+import styles from '../../styles/components/WaitlistTileCard.module.css';
+import {
+  H1, H2, H3, B1,
+} from '../ui/typography';
+
+function parseTerms(courseTerms) {
+  return courseTerms.join(', ');
+}
+
+function WaitlistCard(props) {
+  const {
+    color,
+    course,
+  } = props;
+  const terms = parseTerms(course.terms);
+  const courseURL = `/courses/${course.courseDepartment}/${course.courseNum}`;
+  return (
+    <div className={styles.card} style={{ background: color.pastel }}>
+      <div className={styles.edit}>
+        <a
+          href=" "
+        >
+          <MdOutlineSettings className="text-2xl text-black group-hover:text-black" />
+        </a>
+      </div>
+      <div className={styles.waitlistCardsContainer}>
+        <div>
+          <H3>
+            {course.courseDepartment}
+            {' '}
+            {course.courseNum}
+          </H3>
+          <H3 color={color.dark}>
+            {' '}
+            {course.courseName}
+          </H3>
+        </div>
+      </div>
+      <div>
+        <H3 color={color.dark}> position on 23s</H3>
+        <H3>
+          {course.waitlistPos}
+          /
+          {course.waitlistTotal}
+        </H3>
+      </div>
+      <div>
+        <H3 color={color.dark}> terms </H3>
+        {terms}
+      </div>
+      <div>
+        <H3 color={color.dark}> notes </H3>
+        <TextField
+          id="notes"
+          defaultValue="Normal"
+          variant="standard"
+          color="secondary"
+        />
+      </div>
+      <div className={styles.bottomButtons}>
+        <div className={styles.buttonContainer}>
+          <a href={courseURL}>
+            <button className={styles.btn} type="button">
+              Course Info Page
+            </button>
+          </a>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.btn} type="button">
+            Remove
+          </button>
+        </div>
+        {/* <div className={styles.buttonContainer}>
+          <div className={styles.button}>
+            <RemoveWaitlist />
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default WaitlistCard;
--- pages/waitlist/index.jsx
+++ pages/waitlist/index.jsx
@@ -1,59 +1,135 @@
-import React, { useEffect } from 'react';
-import Head from 'next/head';
-import { useDispatch, useSelector } from 'react-redux';
+// import React, { useEffect } from 'react';
+import React from 'react';
+// import Head from 'next/head';
+// import { useDispatch, useSelector } from 'react-redux';
 import styles from '../../styles/WaitlistHome.module.css';
-import SideNavbar from '../../components/SideNavbar';
-import { H2, B1 } from '../../components/ui/typography';
-import WaitlistModal from '../../components/waitlist/WaitlistModal';
-import { fetchWaitlists } from '../../actions';
+// import SideNavbar from '../../components/SideNavbar';
+import { H2 } from '../../components/ui/typography';
+// import WaitlistModal from '../../components/waitlist/WaitlistModal';
+import WaitlistCard from '../../components/waitlist/WaitlistCard';
+// import { fetchWaitlists } from '../../actions';
+
+const cardColors = [
+  { pastel: '#F9F3FC', dark: '#8E5BA8' },
+  { pastel: '#FCF0E3', dark: '#BA7D37' },
+  { pastel: '#EBF9FA', dark: '#5B8A8D' },
+  { pastel: '#EFFAEB', dark: '#75946A' },
+  { pastel: '#EFE7FA', dark: '#7E5DAC' },
+  { pastel: '#FAEBF6', dark: '#AE5E99' },
+  { pastel: '#F9F3FC', dark: '#8E5BA8' },
+  { pastel: '#FCF0E3', dark: '#BA7D37' },
+];
+
+const waitlistMockData = {
+  courses: [
+    {
+      courseDepartment: 'COSC',
+      courseNum: '52',
+      courseName: 'Full-Stack Web Development',
+      waitlistPos: 10,
+      waitlistTotal: 76,
+      terms: ['23S', '24S', '25S'],
+      joined: 'June 21, 2022',
+      comments: '',
+    },
+    {
+      courseDepartment: 'COSC',
+      courseNum: '74',
+      courseName: 'Machine Learning and Statistical Data Analysis',
+      waitlistPos: 2,
+      waitlistTotal: 14,
+      terms: ['23W'],
+      joined: 'April 10, 2022',
+      comments: '',
+    },
+    {
+      courseDepartment: 'MATH',
+      courseNum: '001',
+      courseName: 'Introduction to Calculus',
+      waitlistPos: 5,
+      waitlistTotal: 7,
+      remainingTerms: 1,
+      terms: ['25F'],
+      joined: 'August 14, 2022',
+      comments: '',
+    },
+    {
+      courseDepartment: 'ECON',
+      courseNum: '001',
+      courseName: 'The Price System: Analysis, Problems, and Policies',
+      waitlistPos: 32,
+      waitlistTotal: 35,
+      remainingTerms: 2,
+      terms: ['23X'],
+      joined: 'November 11, 2022',
+      comments: '',
+    },
+  ],
+};
 
 export default function WaitlistHome() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchWaitlists());
-  }, []);
-  const waitlistContent = useSelector((reduxState) => reduxState.waitlist.current);
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Classy</title>
-        <meta name="description" content="Cool tagline here" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <SideNavbar />
-
-      <H2 className={styles.title}>
-        My Waitlists
-      </H2>
-
-      <main className={styles.main}>
-        <B1 className="description">
-          Join new waitlists from the course info page.
-        </B1>
-
-        <div className={styles.grid}>
-          {waitlistContent.courses ? waitlistContent.courses.map((course, index) => (
-            <WaitlistModal 
-            course={course}
-            studentId={`ObjectId('${waitlistContent.student._id}')`}
-            onWaitlist={true}
-            index={index}
-            entryPoint={'waitlist'}/>
-          )) : ''}
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="_blank"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Classy
-
-        </a>
-      </footer>
+      <H2 color="#14121D">Waitlisted Courses</H2>
+      <p color="#14121D">Join a new waitlist from the course info page.</p>
+      <div className={styles.waitlistCardsContainer}>
+        {waitlistMockData.courses.map((course, i) => (
+          <WaitlistCard course={course} color={cardColors[i % cardColors.length]} />
+        ))}
+      </div>
     </div>
   );
 }
+// export default function WaitlistHome() {
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(fetchWaitlists());
+//   }, []);
+//   const waitlistContent = useSelector((reduxState) => reduxState.waitlist.current);
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Classy</title>
+//         <meta name="description" content="Cool tagline here" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <SideNavbar />
+
+//       <H2 className={styles.title}>
+//         My Waitlists
+//       </H2>
+
+//       <main className={styles.main}>
+//         <B1 className="description">
+//           Join new waitlists from the course info page.
+//         </B1>
+
+//         <div className={styles.grid}>
+//           {waitlistContent.courses ? waitlistContent.courses.map((course, index) => (
+//             <WaitlistModal
+//               course={course}
+//               // studentId={`ObjectId('${waitlistContent.student._id}')`}
+//               // eslint-disable-next-line no-underscore-dangle
+//               studentId={`ObjectId('${waitlistContent.student._id}')`}
+//               onWaitlist
+//               index={index}
+//               entryPoint="waitlist"
+//             />
+//           )) : ''}
+//         </div>
+//       </main>
+
+//       <footer className={styles.footer}>
+//         <a
+//           href="_blank"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Classy
+
+//         </a>
+//       </footer>
+//     </div>
+//   );
+// }
