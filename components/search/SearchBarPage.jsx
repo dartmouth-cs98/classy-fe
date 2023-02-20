e1e7bc996e920c6c6f05461ae78b80d089b839db
Alex Feng <Alex.feng.23@dartmouth.edu>
2023-02-19 20:21:17 -0500

    updated conditional rendering on search

--- components/search/SearchBarPage.jsx
+++ components/search/SearchBarPage.jsx
@@ -6,17 +6,9 @@ import React, {
 } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-import { IconButton, Checkbox } from '@mui/material';
-import CourseData from '../../data/data';
 import TabBar from './TabBar';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
-import Modal from '../Modal';
-import {
-  H2, H3, H4, B1,
-} from '../ui/typography';
-import { distribs, wcs } from '../../constants/colors';
-import { addDistribFilter, removeDistribFilter } from '../../actions';
 import FilterModal from './FilterModal';
 
 function SearchBarPage(props) {
@@ -26,7 +18,23 @@ function SearchBarPage(props) {
   // const inputRef = useRef(null);
   const { children } = props;
   const searchQuery = useSelector((reduxState) => reduxState.search.searchQuery);
-  const searchResults = useSelector((reduxState) => reduxState.search.searchResults);
+  const searchReducer = useSelector((reduxState) => reduxState.search);
+
+  const [filtersApplied, setFiltersApplied] = useState(false);
+  const [filterIconColor, setFilterIconColor] = useState('var(--navy)');
+
+  useEffect(() => {
+    const filtered = (searchReducer.distribFilters.length !== 0
+    || searchReducer.wcFilters.length !== 0
+    || searchReducer.offeredNext
+    || searchReducer.nrEligible);
+    setFiltersApplied(filtered);
+    if (filtered) {
+      setFilterIconColor('var(--navy)');
+    } else {
+      setFilterIconColor('var(--light-grey)');
+    }
+  }, [searchReducer]);
 
   return (
     <div style={{ padding: '20px 80px 50px 275px' }}>
@@ -35,15 +43,15 @@ function SearchBarPage(props) {
       {
         tab === 'Courses'
           ? (
-            <FilterAltOutlinedIcon style={{ cursor: 'pointer', marginLeft: '5px' }} onClick={() => setSearchModalIsOpen(true)} />
+            <FilterAltOutlinedIcon style={{ cursor: 'pointer', marginLeft: '5px', color: filterIconColor }} onClick={() => setSearchModalIsOpen(true)} />
           )
           : (
             null
           )
       }
 
-      {// if search input or search results exist, show results page
-        searchQuery || searchResults.length !== 0
+      {// if search input or any filters applied, show results page
+        searchQuery || filtersApplied
           ? (
             <div>
               <TabBar tab={tab} setTab={setTab} />
