71856762b0ee7c2ba14a1221f36eae9519380445
Alex Feng <Alex.feng.23@dartmouth.edu>
2023-02-26 23:41:19 -0500

    figured out mui autocomplete

--- components/home/ShoppingModal.jsx
+++ components/home/ShoppingModal.jsx
@@ -1,18 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
-import ClearIcon from '@mui/icons-material/Clear';
-import ClearAllIcon from '@mui/icons-material/ClearAll';
 import Modal from '../Modal';
 import { searchCourses } from '../../actions';
-
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-];
 
 function ShoppingModal(props) {
   const {
@@ -20,7 +13,9 @@ function ShoppingModal(props) {
   } = props;
 
   const [query, setQuery] = useState('');
-  const [value, setValue] = useState(null);
+  const [courseObj, setCourseObj] = useState(null);
+
+  console.log(courseObj);
 
   const dispatch = useDispatch();
   const searchResults = useSelector((reduxState) => reduxState.home.searchResults);
@@ -35,85 +30,29 @@ function ShoppingModal(props) {
     >
       <Autocomplete
         options={searchResults}
+        clearOnBlur={false}
         onInputChange={(event, NewInputValue) => {
           setQuery(NewInputValue);
           dispatch(searchCourses(NewInputValue));
           console.log(NewInputValue);
         }}
-        onChange={(e, newValue) => setValue(newValue)}
+        onChange={(e, newValue) => setCourseObj(newValue)}
         inputValue={query}
-        value={value}
-        getOptionLabel={(option) => option.courseTitle || ''}
-        isOptionEqualToValue={(courseOption, courseValue) => courseOption._id === courseValue._id}
+        value={courseObj}
+        getOptionLabel={(option) => (`${option.courseDept + option.courseNum} - ${option.courseTitle}`) || ''}
+        isOptionEqualToValue={(option, value) => option._id === value._id}
         style={{ width: 300 }}
         renderInput={(params) => (
-          <TextField {...params} label="Combo box" variant="outlined" />
+          <TextField {...params} placeholder="Search Course" variant="outlined" />
+        )}
+        renderOption={(renderProps, option) => (
+          <li {...renderProps} key={option._id}>
+            {option.courseDept + option.courseNum}
+            {' - '}
+            {option.courseTitle}
+          </li>
         )}
       />
-
-      {/* <Autocomplete
-        freeSolo
-        options={searchResults}
-        getOptionLabel={(option) => option._id}
-        // onInputChange={(event, value, reason) => {
-        //   setQuery(value);
-        //   dispatch(searchCourses(value));
-        // }}
-        // value={query}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder="Search Course"
-            InputProps={{
-              ...params.InputProps,
-            }}
-            sx={{ width: '300px' }}
-          />
-        )}
-      /> */}
-      {/* <Autocomplete
-        freeSolo
-        options={searchResults}
-        getOptionLabel={(option) => option._id}
-        // renderOption={(option) => <>{option.courseDept}</>}
-        onChange={(event, value, reason) => {
-          console.log(value);
-          setQuery(value);
-          dispatch(searchCourses(value));
-        }}
-        value={query}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder="Search Course"
-            InputProps={{
-              ...params.InputProps,
-            }}
-            sx={{ width: '300px' }}
-          />
-        )} */}
-      {/* /> */}
-      {/* <Autocomplete
-        freeSolo
-        options={searchResults.map((course, index) => `${course.courseDept
-           + course.courseNum} - ${course.courseTitle}`)}
-        onInputChange={(event, value, reason) => {
-          setQuery(value);
-          dispatch(searchCourses(value));
-        }}
-        value={query}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder="Search Course"
-            InputProps={{
-              ...params.InputProps,
-            }}
-            sx={{ width: '300px' }}
-          />
-        )}
-      /> */}
-
     </Modal>
   );
 }
