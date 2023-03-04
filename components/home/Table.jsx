94b50fc6f8f4782f4f5a47350901106c7d8ebcdc
Alex Feng <Alex.feng.23@dartmouth.edu>
2023-03-04 05:02:25 -0500

    home table style updates

--- components/home/Table.jsx
+++ components/home/Table.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { useDispatch } from 'react-redux';
+import { useRouter } from 'next/router';
 import { B1 } from '../ui/typography';
 import styles from '../../styles/Home.module.css';
 import stylesw from '../../styles/WaitlistDetail.module.css';
@@ -13,73 +14,53 @@ function Table(props) {
     courses, mode, studentId,
   } = props;
   const dispatch = useDispatch();
-  const removeCourse = (courseId) => {
-    if (mode === 'cart') {
-      dispatch(markCourse(studentId, courseId, 'cart', true));
-    } else if (mode === 'completed') {
-      dispatch(markCourse(studentId, courseId, 'taken', true));
-    }
-  };
+  const router = useRouter();
 
-  const loadCourses = () => courses.map((course) => {
-    console.log('course', course);
+  if (courses?.length > 0) {
     return (
-      <tr>
-        <td>
-          <B1>
-            <Link href={`/courses/${course.courseDept}/${course.courseNum}`}>
-              {`${course.courseDept} ${course.courseNum}`}
-            </Link>
-          </B1>
-        </td>
-        <td>
-          <Link href={`/courses/${course.courseDept}/${course.courseNum}`}>
-            <B1>{course.courseTitle}</B1>
-          </Link>
-        </td>
-        <td>
-          <Link href="/home">
-            <button
-              type="button"
-              className={stylesw.button}
-              onClick={() => removeCourse(course._id)}
-            >
-              X
-            </button>
-          </Link>
-        </td>
-      </tr>
-    );
-  });
-
-  const tableOrEmpty = () => {
-    if (courses?.length > 0) {
-      return (
+      <div className={styles.table}>
         <table>
-          <thead>
-            <th>Course Code</th>
-            <th>Course Title</th>
-            <th>Remove</th>
-          </thead>
-          <tbody>{loadCourses()}</tbody>
+          <tbody>
+            {courses.map((course) => (
+              <tr onClick={() => router.push(`/courses/${course.courseDept}/${course.courseNum}`)} style={{ cursor: 'pointer' }}>
+                <td align="left" style={{ minWidth: '130px' }}>
+                  <B1>
+                    {`${course.courseDept} ${course.courseNum}`}
+                  </B1>
+                </td>
+                <td style={{
+                  maxWidth: '450px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap',
+                }}
+                >
+                  <B1>
+                    {course.courseTitle}
+                    {' '}
+                    sdfsdfsdfsdfsdf
+                  </B1>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      );
-    }
-    if (mode === 'cart') {
-      return <B1>Add courses to your shopping cart</B1>;
-    }
-    if (mode === 'completed') {
-      return <B1>Mark courses as completed</B1>;
-    }
-    return '';
-  };
-  return (
-    <div
-      className={styles.table}
-    >
-      {tableOrEmpty()}
-    </div>
-  );
+      </div>
+
+    );
+  }
+  if (mode === 'cart') {
+    return (
+      <div className={styles.table}>
+        <B1>Add courses to your shopping cart</B1>
+      </div>
+    );
+  }
+  if (mode === 'completed') {
+    return (
+      <div className={styles.table}>
+        <B1>Mark courses as completed</B1>
+      </div>
+    );
+  }
+  return '';
 }
 
 export default Table;
