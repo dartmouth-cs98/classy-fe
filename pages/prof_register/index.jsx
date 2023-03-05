4c127779cdfdf16910b3764504e5b23d124fbefe
tradewinds23 <alyssa.k.anderson.23@dartmouth.edu>
2023-03-05 08:55:50 -0500

    initial pages for prof registration
    Initial page structure for the workflow. Professor registration pages based on student registration.

---
+++ pages/prof_register/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { AuthProvider } from './AuthProvider';
+import Register from '../../components/register/ProfRegister';
+// import { H2, B1 } from '../../components/ui/typography';
+import styles from '../../styles/Login.module.css';
+
+export default function ProfRegister() {
+  return (
+    <div className={styles.container}>
+      <main className="RegisterHome">
+        <Register />
+      </main>
+    </div>
+  );
+}
