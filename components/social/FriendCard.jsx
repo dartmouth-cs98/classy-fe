2e3d11ec1eec43eeebcbff6b19daf53169f9d23c
Henry Kim <henry.kim.23@dartmouth.edu>
2023-03-05 17:10:34 -0500

    fix load majors

--- components/social/FriendCard.jsx
+++ components/social/FriendCard.jsx
@@ -11,42 +11,76 @@ import { defaultUserImageURL } from '../../constants/mockData';
 
 function FriendCard(props) {
   const { student, forSearch } = props;
+  console.log('friend card', student.majors);
+  const loadMajors = () => student?.majors?.map((major, i) => (
+    <B1>
+      {i + 1 === student.majors.length
+        ? `${major.name} Major`
+        : `${major.name} Major • `}
+    </B1>
+  ));
+
+  const loadMinors = () => student?.minors?.map((minor, i) => (
+    <B1>
+      {i + 1 === student.minors.length
+        ? `${minor.name} Major`
+        : `${minor.name} Major • `}
+    </B1>
+  ));
 
   return (
     <div>
       {forSearch ? (
         <Link href={`/social/friendProfile/${student.student}`}>
           <div>
-            <img className={styles.pic} src={student?.profileImageUrl?.length > 0 ? student?.profileImageUrl : defaultUserImageURL} alt="friend pic" />
+            <img
+              className={styles.pic}
+              src={
+								student?.profileImageUrl?.length > 0
+								  ? student?.profileImageUrl
+								  : defaultUserImageURL
+							}
+              alt="friend pic"
+            />
             <div className={styles.description}>
-              <H3 style={{ margin: '0px' }}>{`${student.firstName} ${student.lastName} '${student.studentObj[0].classYear}`}</H3>
-              <div className="majorMinorContainer">
-                {student?.majors?.map((major, i) => (
-                  <B1>{i + 1 === student.studentObj[0].majors.length ? `${major.name} Major` : `${major.name} Major • `}</B1>
-                ))}
-              </div>
-              <div className="majorMinorContainer">
-                {student?.minors?.map((minor, i) => (
-                  <B1>{i + 1 === student.studentObj[0].minors.length ? `${minor.name} Minor` : `${minor.name} Minor • `}</B1>
-                ))}
-              </div>
+              <H3
+                style={{ margin: '0px' }}
+              >
+                {`${student.firstName} ${student.lastName} '${student.studentObj[0].classYear}`}
+
+              </H3>
+              <div className="majorMinorContainer">{loadMajors()}</div>
+              <div className="majorMinorContainer">{loadMinors()}</div>
             </div>
           </div>
         </Link>
       ) : (
         <Link href={`/social/friendProfile/${student._id}`}>
           <div className={styles.container}>
-            <img className={styles.pic} src={student?.user?.profileImageUrl?.length > 0 ? student.user.profileImageUrl : defaultUserImageURL} alt="friend pic" />
+            <img
+              className={styles.pic}
+              src={
+								student?.user?.profileImageUrl?.length > 0
+								  ? student.user.profileImageUrl
+								  : defaultUserImageURL
+							}
+              alt="friend pic"
+            />
             <div className={styles.description}>
-              <H3 style={{ margin: '0px' }}>{`${student.user.firstName} ${student.user.lastName} '${student.classYear}`}</H3>
-              <div className="majorMinorContainer">
-                {student.majors.map((major, i) => (
-                  <B1>{i + 1 === student.majors.length ? `${major} Major` : `${major} Major • `}</B1>
-                ))}
-              </div>
+              <H3
+                style={{ margin: '0px' }}
+              >
+                {`${student.user.firstName} ${student.user.lastName} '${student.classYear}`}
+
+              </H3>
+              <div className="majorMinorContainer">{loadMajors()}</div>
               <div className="majorMinorContainer">
                 {student.minors.map((minor, i) => (
-                  <B1>{i + 1 === student.minors.length ? `${minor} Minor` : `${minor} Minor • `}</B1>
+                  <B1>
+                    {i + 1 === student.minors.length
+										  ? `${minor} Minor`
+										  : `${minor} Minor • `}
+                  </B1>
                 ))}
               </div>
             </div>
