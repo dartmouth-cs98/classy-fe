7fc435d7aa2cf1ed1e1731eaf5152ae76b8965e2
gyur2hwng <gyuri.hwang.23@dartmouth.edu>
2023-03-06 22:47:18 -0500

    added prof nav bar

--- pages/prof_home/index.jsx
+++ pages/prof_home/index.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Image from 'next/image';
+import {
+  MdOutlineHome,
+  MdOutlineLogout,
+} from 'react-icons/md';
 import { fetchProfessorHome } from '../../actions';
 import CourseSimple from '../../components/profWaitlist/CourseSimple';
 import {
   A, H3, H1, B1, H4,
 } from '../../components/ui/typography';
 import styles from '../../styles/components/ProfHome.module.css';
+import logo from '../../images/logo.png';
+import NavbarLink from '../../components/NavbarLink';
 
 const cardColors = [
   { pastel: '#FCF0E3', dark: '#BA7D37' },
@@ -25,6 +32,8 @@ function ProfHome() {
     dispatch(fetchProfessorHome(name));
   }, []);
 
+  const sidenavIconStyles = 'text-2xl text-white group-hover:text-black ';
+
   const currentProfHome = useSelector((reduxState) => reduxState.profHome.current);
   if (!currentProfHome) {
     dispatch(fetchProfessorHome(name));
@@ -34,11 +43,38 @@ function ProfHome() {
   const pic = 'https://web.cs.dartmouth.edu/sites/department_computer_science/files/styles/profile_portrait/public/LorieLoeb.png?itok=A6088OY8';
   return (
     <div>
-      <div className={styles.nav}>
-        <H4 color="white" style={{ alignItems: 'center' }}> Classy </H4>
+      <div className={styles.nav} />
+      <div className="p-6 w-1/2 h-screen bg-black z-20 fixed top-0 -left-96 lg:w-60 lg:left-0 peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+        <div className="flex flex-col justify-start items-center my-6">
+          <Image src={logo} width={60} height={60} alt="classy logo" />
+          <H4
+            color="white"
+            className="text-base text-center font-bold pb-4 w-full mt-3.5"
+          >
+            Classy
+          </H4>
+
+          {/* main tabs */}
+          <div className="my-60 pb-10 w-full">
+            <NavbarLink
+              link="home"
+              title="Home"
+              icon={<MdOutlineHome className={sidenavIconStyles} />}
+            />
+
+            {/* logout */}
+            <div className="my-60 w-full">
+              <NavbarLink
+                link=""
+                title="Logout"
+                icon={<MdOutlineLogout className={sidenavIconStyles} />}
+              />
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div style={{ padding: '20px 80px 50px 275px' }}>
+      <div className={styles.main}>
         <div style={{
           display: 'flex', flexDirection: 'row', alignItems: 'flex-end', gap: '20px',
         }}
@@ -64,7 +100,6 @@ function ProfHome() {
                 key={course?.courseName}
                 course={course}
                 color={cardColors[i % cardColors.length]}
-                type="prof"
               />
             ))}
           </div>
